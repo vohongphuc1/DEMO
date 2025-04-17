b3da36d983d536a2e0f61fab1a56b9ae71e009b1
Võ Hồng Phúc <vohongphucnt05@gmail.com>
2025-04-17 18:06:11 +0700

    lan 1

---
+++ Student/src/main/java/com/mycompany/student/Student.java
@@ -0,0 +1,19 @@
+package com.mycompany.student;
+public class Student {
+    private String studentId;
+    private String name;
+    private int age;
+
+    public Student(String studentId, String name, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getStudentId() { return studentId; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public void updateName(String newName) { this.name = newName; }
+}
